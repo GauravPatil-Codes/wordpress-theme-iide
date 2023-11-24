fa9dcff163e7cbf5d24492d9ca084e81cce868a1
GauravPatil-Codes <gauravpatil754@gmail.com>
2023-11-24 13:14:22 +0530

    Wordpress theme for IIDE

---
+++ wp-content/theme/scripts.php
@@ -0,0 +1,13 @@
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script src="https://cdn.tailwindcss.com"></script>
+<script>
+  tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        }
+      }
+    }
+  }
+</script>
